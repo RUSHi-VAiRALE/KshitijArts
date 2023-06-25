6533ba6de34078dbe5050aafc5951bf309bbf8d8
RUSHi-VAiRALE <rushivairale932@gmail.com>
2023-06-26 00:12:18 +0530

    Account DashBoard page added

--- src/components/accDash.js
+++ src/components/accDash.js
@@ -6,6 +6,30 @@ const ADashBoard = () =>{
         <div className="aDashName">
             MY DASHBOARD
         </div>
+        <div className="aDashMain">
+            <div className="aDashInfo">
+                <div className="aDashInfo1">
+                    <div style={{
+                        fontWeight:"bold",
+                        background:"lightGray"
+                    }} className="aDashDiv1">
+                        Account Dashboard
+                    </div>
+                    <div className="aDashDiv1">
+                        Account Information
+                    </div>
+                    <div className="aDashDiv1">
+                        Address Book
+                    </div>
+                    <div className="aDashDiv1">
+                        My Orders
+                    </div>
+                </div>
+                <button className="LogButton">
+                    <p className="btnName">LOG OUT</p>
+                </button>
+            </div>
+        </div>
     </div>
 )
 }
