63c057dc3d2a0d069cf676846cfc4dce55a1f2b3
RUSHi-VAiRALE <rushivairale932@gmail.com>
2024-01-26 20:29:26 +0530

    new updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -98,7 +98,7 @@ const Navbar = () => {
                   onClick={()=>{
                     navigate('/login')
                   }}
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base cursor-pointer font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Log in <span aria-hidden="true">&rarr;</span>
                 </a>
@@ -198,7 +198,7 @@ const Navbar = () => {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right cursor-pointer rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
                           <a
